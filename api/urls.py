e0e4afd0e8e71eed7c55a9deef8730f5fe7f7813
Mansi Joshi <115941577+mansijosh@users.noreply.github.com>
2024-01-12 22:36:20 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
+# urls.py
 from django.urls import path
-from . import views
+from .views import create_form, add_responses
 
-urlpatterns =  [
-    path('api/create_collection/', views.create_collection, name='create_collection'),
-    path('api/add_questions/', views.add_questions, name='add_questions'),
-
+urlpatterns = [
+    path('create_form/', create_form, name='create_form'),
+    path('add_responses/', add_responses, name='add_responses'),
+    # Add more URLs as needed
 ]
